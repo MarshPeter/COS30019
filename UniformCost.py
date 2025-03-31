424f55b8addf8c07b07b3540ce049f0bc339acad
MarshPeter <peter.m.farmer.204@gmail.com>
2025-03-31 22:47:30 +1100

    commented multiple stuff

--- UniformCost.py
+++ UniformCost.py
@@ -1,36 +1,50 @@
 import heapq
 from Graph import Graph
 
+# currently in classes, maybe not useful
 class UniformCost:
 
     def __init__(self, graph: Graph):
         self.graph = graph
 
     def uniform_cost_search(self):
-        frontier = []
+        frontier = [] 
+        # this adds to the frontier such that the next pop will be the min cost
+        # order of list is: [cost, origin, path]
         heapq.heappush(frontier, [0, self.graph.origin, [self.graph.origin]]) 
+
+        # keep track of nodes we have checked
         explored = {}
+
+        # dictionary of all destinations + lowest costs we have found
+        # in form of {destination: (path, cost)}
         found_destinations = {}
 
         while frontier:
+            # removes item with smallest cost, reorganizes so that smallest cost is next again
             cost, node, path = heapq.heappop(frontier)
-            print(node, self.graph.goals)
 
+            # we found a destinatioon
             if self.graph.is_goal(node):
-                print("TEST")
+                # add a new destination if not seen before
                 if node not in found_destinations:
                     found_destinations[node] = (path, cost)
+                # if we have found all destinations, then return
                 if len(found_destinations) == len(self.graph.goals):
-                    return found_destinations.items()
+                    return found_destinations.items() # return items for now, maybe change later
 
+            # if the current path to a node is more than what we have found previously
+            # discard this path, it will not be optimal
             if node in explored and explored[node] <= cost:
                 continue
 
+            # add or update cost of path to node
             explored[node] = cost
 
-            # returns List(Tuple(node_neighbour_number, edge_cost))
+            # returns List(Tuple(node_neighbour_number, edge_cost)) for the current node
             edges = self.graph.get_edges(node)
 
+            # add all edges to the heap in form: [cost, origin, path]
             for edge in edges:
                 cumulative = cost + edge[1]
                 new_path = path + [edge[0]]
--- main.py
+++ main.py
@@ -2,36 +2,41 @@ from UniformCost import UniformCost
 from Graph import Graph
 from enum import Enum
 
-
+# Current stage of a graph being read
 class FileReadStage(Enum):
-    START = 0
-    NODES = 1
-    EDGES = 2
-    ORIGIN = 3
-    DESTINATIONS = 4
-    END = 5
+    START = 0 # we are starting a new graph
+    NODES = 1 # we are reading nodes
+    EDGES = 2 # we are reading edges
+    ORIGIN = 3 # we are reading the origin
+    DESTINATIONS = 4 # we are reading the destinations
+    END = 5 # We have completed the graph
 
 print("Hello, World!")
 
+# returns a list of Graph objects based of file read
 def create_graphs():
     graphs = []
     graph = Graph()
     stage = FileReadStage.START
+
+    # read file, auto close when at EOF
     with open('./multi_tests.txt', 'r') as file:
         for line in file:
-            #print(line.strip())
-            data = line.strip()
-            print(stage)
+            data = line.strip() # remove whitespace
 
+            # If this file holds multiple graphs, this starts a new graph after one is finished
             if stage == FileReadStage.END and data == "Nodes:":
                 graph = Graph()
                 stage = FileReadStage.START
 
             stage, changed = update_stage(stage, data)
-
+            
+            # Stage of graph read has changed, but we need new line for data
             if changed: continue
 
             if stage is FileReadStage.NODES:
+                # Read in nodes and add them to current graph
+                # All nodes in form: 1: (4, 1)
                 parts = line.split(":")
                 neighbor_edge = parts[1].strip()
                 node_number = int(parts[0])
@@ -39,26 +44,26 @@ def create_graphs():
                 node_y = int(neighbor_edge[-2])
                 graph.add_node(node_number, node_x, node_y)
             elif stage is FileReadStage.EDGES:
+                # read in edges and add them to current graph
+                # all edges in form: (2,1): 2
                 parts = line.split(":")
                 node_one = int(parts[0][1])
                 node_two = int(parts[0][3])
                 cost = int(parts[1][1])
                 graph.add_neighbor(node_one, node_two, cost)
             elif stage is FileReadStage.ORIGIN:
+                # read in origin and add to current graph
+                # all origins in form: 1
                 graph.set_origin(int(line[0]))
             elif stage is FileReadStage.DESTINATIONS:
+                # read in destinations and add to current graph
+                # all destinations are in form: 5; 4
                 parts = [int(x.strip()) for x in line.split(";")]
                 graph.set_goals(parts)
                 stage = FileReadStage.END
                 graphs.append(graph)
 
-            # print(graph.node_positions.items())
-            # print(graph.edges)
-            # print(graph.origin)
-            # print(graph.goals)
-
     return graphs
-
 
 def update_stage(current_stage, line):
     if line == "Nodes:":
@@ -71,7 +76,6 @@ def update_stage(current_stage, line):
         return FileReadStage.DESTINATIONS, True
     return current_stage, False
 
-
 graphs = create_graphs()
 solutions = []
 
@@ -79,6 +83,4 @@ for graph in graphs:
     solution = UniformCost(graph)
     solutions.append(solution.uniform_cost_search())
 
-# solution = UniformCost(graph)
-# print(solution.uniform_cost_search())
 print(solutions)
