a44a678087826b37404e9d0cfc47ff15d34a50ab
MarshPeter <peter.m.farmer.204@gmail.com>
2025-04-12 19:36:33 +1000

    Graph comments complete

--- Graph.py
+++ Graph.py
@@ -1,36 +1,33 @@
 from collections import defaultdict
 
 class Graph:
-    """
-    Contains the state of a map, node that all nodes on the graph are 1-based all values start uninitialized
-    Member Variables:
-    origin (int || None): The origin node
-    goals (List[int] || None): A list of all possible goals
-    nodes (dict(int, List[Tuple(int, int)]) || None): A dictionary that contains all nodes on a map represented
-        by their numbers (whihc are 1-based) that maps to their neighbors as a List of Tuples. The first
-        element is the neighbor node number, the second variable is the cost to reach that node
-    """
     def __init__(self):
-        self.origin = None
-        self.goals = None
-        self.edges = defaultdict(list)
-        self.node_positions = defaultdict(tuple) # {node_number: x, y}
+        self.origin = None # will become a number
+        self.goals = None # will be a list of numbers
+        self.edges = defaultdict(list) # will be set up such that: {node: [(neighbor1, cost), (neighbor2, cost)...]}
+        self.node_positions = defaultdict(tuple) # {node_number: (x, y)}
 
+    # sets the position of a number {node_number: (x, y)}
     def add_node(self, node, pos_x, pos_y):
         self.node_positions[node] = (pos_x, pos_y)
 
+    # sets the origin node
     def set_origin(self, origin):
         self.origin = origin
 
+    # sets goals to a list of goals 
     def set_goals(self, goals):
         self.goals = goals
 
+    # adds an edge to the graph {node: [(neighbor1, cost), (neighbor2, cost)...]}
     def add_neighbor(self, node, neighbor_node, cost):
         self.edges[node].append((neighbor_node, cost))
 
+    # returns true if the parameter node number is in the goals list
     def is_goal(self, node):
         return node in self.goals
 
+    # return all neighbors and costs of a node
     def get_edges(self, node):
         return self.edges[node]
 
--- main.py
+++ main.py
@@ -3,29 +3,42 @@ import sys
 from Printing import printAStar, printBFS, printDFS, printGreedy, printRecursiveBestFirstSearch, printUniformCost
 from graphReader import create_graphs
 
+# methods available to use for solution
 solutions = {
     "bfs": printBFS,
     "dfs": printDFS,
     "greedy": printGreedy,
     "astar": printAStar,
     "uniform": printUniformCost,
-    "rbfs": printRecursiveBestFirstSearch
+    "rbfs": printRecursiveBestFirstSearch # per report, this one isn't entirely accurate, but won't break or infinite cycle
 }
 
 def main():
+    # Confirms format + provides help info
     if len(sys.argv) != 3:
         print("You need to use the following command:\n\npython main.py <filename> <method>")
         print("\nExample:\n\npython main.py ./tests.txt bfs\n")
+        print("\nOptions:")
+        print("bfs: Breadth First Search")
+        print("dfs: Depth First Search")
+        print("greedy: Greedy Best First Search")
+        print("astar: A* Search")
+        print("uniform: Uniform-Cost Search")
+        print("rbfs: Recursive Best First Search\n")
         print("\nAlternatively, use the following to see result of all method for you tests case:")
-        print("python main.py <filename> all")
+        print("python main.py <filename> all\n")
         return
 
+    # assumes user inputs correct information
     tests = sys.argv[1]
     solution_type = sys.argv[2]
 
+    # retrieve all graphs of test case (provided ones are one off test cases, but can handle multiple graphs in a single test case)
     graphs = create_graphs(tests)
 
     print(f"{tests} {solution_type}")
+    
+    # for easier comparison
     if solution_type == "all":
         solutions["bfs"](graphs)
         print()
@@ -39,8 +52,10 @@ def main():
         print()
         solutions["rbfs"](graphs)
     elif solution_type in solutions.keys():
+        # specific solution
         solutions[solution_type](graphs) 
     else:
+        # defaults to bfs output
         print("Solution method not recognized, running in BFS")
         solutions["bfs"](graphs)
     
